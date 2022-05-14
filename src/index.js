2197116c2e9df3e1f0cffe43657c2bd7e479d084
Animesh garai <animeshgarai09@gmail.com>
2022-05-14 11:01:11 +0530

    backend added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import App from "./app";
 import './global/css/global.scss'
 import 'react-felix-ui/dist/cjs/index.css'
 import { FelixProvider } from "react-felix-ui"
+import { makeServer } from "./server";
+
+// Call make Server
+makeServer();
 
 ReactDOM.render(
     <React.StrictMode>
